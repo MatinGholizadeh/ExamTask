ab481913aede8acfebf476c9f948a9c8a047374e
Matin gholizadeh <matin.gh.work@gmail.com>
2025-07-29 01:19:04 +0330

    fixing rtl

--- src/ExamTask.Presentation/ExamTask.Presentation/Pages/Register.cshtml.cs
+++ src/ExamTask.Presentation/ExamTask.Presentation/Pages/Register.cshtml.cs
@@ -1,8 +1,8 @@
-﻿using ExamTask.Application.Features.Auth.Register.Commands;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using ExamTask.Application.Features.Auth.Register.Commands;
 
 namespace ExamTask.Presentation.Pages;
 
